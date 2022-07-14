37fff98caea8054003a8ad7b4f61484708ab8305
AlgonzaDJun <algonza.20050@mhs.unesa.ac.id>
2022-07-14 19:55:07 +0700

    sort

--- src/containers/MovieList.js
+++ src/containers/MovieList.js
@@ -7,26 +7,43 @@ import {useSearchParams} from 'react-router-dom'
 
 const MovieList = () => {
   const [movies, setMovies] = useState([]);
+  const [queryParams, setQueryParams] = useSearchParams()
+  const [isReady, setReady] = useState(false)
 
   useEffect(() => {
     const fetchMovies = async () => {
       try {
         const fetchedMovies = await tmdb.get("trending/movie/week");
         setMovies(fetchedMovies.data.results);
+        setReady(true)
       } catch (error) {
         console.log(error);
       }
     };
 
     fetchMovies();
-  }, []);
+  }, [isReady]);
 
-  const [queryParams, setQueryParams] = useSearchParams()
 
   const setSortParams = (type) => {
     queryParams.set("sort", type)
     setQueryParams(queryParams)
   }
+
+  useEffect(() => {
+    if(!isReady) return;
+    const sortMovies = (type) => {
+      if(type === "asc") {
+        const sortedAsc = movies.sort(((a, b) => a.vote_average - b.vote_average));
+        setMovies(sortedAsc)
+      }
+      if(type === "desc") {
+        const sortedDesc = movies.sort(((a, b) => b.vote_average - a.vote_average));
+        setMovies(sortedDesc)
+      }
+    }
+    sortMovies(queryParams.get('sort'))
+  }, [queryParams, isReady])
 
   return (
     <>
@@ -36,7 +53,7 @@ const MovieList = () => {
         mt:10}}>
         Sort By Rating
         <Button onClick={() => setSortParams("asc")}>Asc</Button>
-        <Button onClick={() => setSortParams("desc")}>Dsc</Button>
+        <Button onClick={() => setSortParams("desc")}>Desc</Button>
       </Box>
       <Box
         sx={{
