a5f537f482b43ea886e1cd57ec4ce1d0e41765c7
AlgonzaDJun <algonza.20050@mhs.unesa.ac.id>
2022-07-14 13:32:01 +0700

    with-routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import './App.css';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "./App.css";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
-import { ThemeProvider } from '@mui/material';
+import { Box, ThemeProvider } from "@mui/material";
 
-import Navbar from './components/Navbar';
-import MovieList from './containers/MovieList';
-import theme from './themes/theme';
+import Navbar from "./components/Navbar";
+import MovieList from "./containers/MovieList";
+import theme from "./themes/theme";
+import { Route, Routes } from "react-router-dom";
+import About from "./containers/About";
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
       <div className="App">
-        <Navbar></Navbar>
-        <MovieList></MovieList>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<MovieList />} />
+          <Route path="about" element={<About />} />
+          <Route path="indonesian" element={<div style={{margin: "70px"}}>Indonesian</div>} />
+          <Route path="pricing" element={<div style={{margin: "70px"}}>Pricing</div>} />
+        </Routes>
       </div>
     </ThemeProvider>
   );
-}
+};
 
 export default App;
---
+++ src/containers/About.js
@@ -0,0 +1,12 @@
+import { Box } from '@mui/material'
+import React from 'react'
+
+const About = () => {
+  return (
+    <Box sx={{ margin: 10 }}>
+              Halaman About
+            </Box>
+  )
+}
+
+export default About
