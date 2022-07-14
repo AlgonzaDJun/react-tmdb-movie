bb5d6d90a2062f7c31c156d685a93808b9aa9cb7
AlgonzaDJun <algonza.20050@mhs.unesa.ac.id>
2022-07-14 15:16:10 +0700

    query params

--- src/containers/MovieList.js
+++ src/containers/MovieList.js
@@ -1,39 +1,57 @@
-import { Box } from "@mui/material";
-import axios from "axios";
+import { Box, Button } from "@mui/material";
 import { useEffect, useState } from "react";
-import tmdb from "../apis/tmdb";
 
+import tmdb from "../apis/tmdb";
 import MovieCard from "../components/MovieCard";
-// import fetchedMovies from "../data/fetchedMovies.json";
+import {useSearchParams} from 'react-router-dom'
 
 const MovieList = () => {
   const [movies, setMovies] = useState([]);
 
-  const fetchData = async () => {
-    // You can await here
-    const response = await tmdb.get("trending/movie/week");
-    setMovies(response.data.results);
-  };
-
   useEffect(() => {
-    fetchData();
+    const fetchMovies = async () => {
+      try {
+        const fetchedMovies = await tmdb.get("trending/movie/week");
+        setMovies(fetchedMovies.data.results);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchMovies();
   }, []);
 
+  const [queryParams, setQueryParams] = useSearchParams()
+
+  const setSortParams = (type) => {
+    queryParams.set("sort", type)
+    setQueryParams(queryParams)
+  }
 
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "row",
-        flexWrap: "wrap",
-        justifyContent: "space-between",
-        mt: 5,
-      }}
-    >
-      {movies.map((movie) => (
-        <MovieCard movie={movie}></MovieCard>
-      ))}
-    </Box>
+    <>
+      <Box sx={{
+        display:'flex',
+        justifyContent:'flex-end',
+        mt:10}}>
+        Sort By Rating
+        <Button onClick={() => setSortParams("asc")}>Asc</Button>
+        <Button onClick={() => setSortParams("desc")}>Dsc</Button>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "space-between",
+          mt: 5,
+        }}
+      >
+        {movies.map((movie) => (
+          <MovieCard movie={movie}></MovieCard>
+        ))}
+      </Box>
+    </>
   );
 };
 
